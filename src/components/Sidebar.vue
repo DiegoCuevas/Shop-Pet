cdeeff9ed4857502624c01b9a823bfffc7ca6780
Diego Cuevas <diegocuevas579@gmail.com>
2022-01-14 10:22:48 -0500

    ADD: get categories

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class=" text-center text-2xl my-5">Categorias: </div>
+    <div v-for="(e, k) in categories" :key="k" class="">{{e.name}}</div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Home",
+  props: {
+    categories: [],
+  },
+  mounted() {
+    this.getCategories();
+  },
+  methods: {
+    getCategories() {
+      this.axios
+        .get("http://sva.talana.com:8000/api/product-category/")
+        .then((response) => {
+          this.categories = response.data;
+        });
+    },
+  },
+};
+</script>
